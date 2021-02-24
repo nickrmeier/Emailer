844ea9fd1a25e11ab427932924385fcc90910b55
nickrmeier <nickrmeier@gmaill.com>
2021-02-24 14:16:56 -0800

    added billing and client side app

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return(
+        <div style={{ textAlign: 'center' }}>
+            <h1>This is the Dashboard!</h1>
+            <p>Here are your surveys</p>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div style={{ textAlign: 'center' }}>
+            <h1>Welcome to Emailer!</h1>
+            <p>Your premier source for surveys</p>
+        </div>
+    )
+}
+
+export default Landing;
